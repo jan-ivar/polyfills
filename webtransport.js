9f2683f22dd6832582bb7498d7189d4e5d2f0f9e
Jan-Ivar Bruaroey <wingboy@gmail.com>
2021-01-23 20:08:13 -0500

    Polyfill WebTransport constructor + ReadableStream async iterator

---
+++ webtransport.js
@@ -0,0 +1,16 @@
+WebTransport = WebTransport || QuicTransport;
+
+if (!ReadableStream.prototype[Symbol.asyncIterator]) {
+  ReadableStream.prototype[Symbol.asyncIterator] = function() {
+    const stream = this;
+    return {
+      reader: null,
+      next() {
+        this.reader ||= stream.getReader();
+        const result = this.reader.read();
+        if (result.done) reader.releaseLock();
+        return result;
+      }
+    };
+  }
+}
