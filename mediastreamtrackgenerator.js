8fc3331b5d850513be7f3cd127d65d9f1dda486e
Jan-Ivar Bruaroey <jan-ivar@users.noreply.github.com>
2024-09-13 15:51:25 -0400

    fix

--- mediastreamtrackgenerator.js
+++ mediastreamtrackgenerator.js
@@ -3,21 +3,11 @@ if (!window.MediaStreamTrackGenerator) {
     constructor() {
       const canvas = document.createElement("canvas");
       const ctx = canvas.getContext('2d', {desynchronized: true});
-      const stream = canvas.captureStream();
-      this.track = stream.getVideoTracks()[0];
-      let frames = 0, fps = 0, f1 = 0, t1 = performance.now();
+      this.track = canvas.captureStream().getVideoTracks()[0];
       this.writable = new WritableStream({
         write: frame => {
-          frames++;
-          const now = performance.now();
-          if (now - t1 > 1000) {
-            fps = (frames - f1) / ((now - t1) / 1000);
-            f1 = frames;
-            t1 = now;
-          }
-          const {displayWidth, displayHeight} = frame;
-          canvas.width = displayWidth;
-          canvas.height = displayHeight;
+          canvas.width = frame.displayWidth;
+          canvas.height = frame.displayHeight;
           ctx.drawImage(frame, 0, 0, canvas.width, canvas.height);
           frame.close();
         },
