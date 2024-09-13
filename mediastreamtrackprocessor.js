dd19e42288cb1c5dcbe9802101600355b8e3e3ea
Jan-Ivar Bruaroey <jan-ivar@users.noreply.github.com>
2024-09-13 17:30:30 -0400

    fix

--- mediastreamtrackprocessor.js
+++ mediastreamtrackprocessor.js
@@ -3,28 +3,22 @@ if (!self.MediaStreamTrackProcessor) {
     constructor({track}) {
       const video = document.createElement("video");
       video.srcObject = new MediaStream([track]);
-      const loaded = Promise.all([
-        video.play(),
-        new Promise(r => video.onloadedmetadata = r)
-      ]);
-
+      const loaded = Promise.all([video.play(), new Promise(r => video.onloadedmetadata = r)]);
       this.track = track;
-      const interval = 1000 / track.getSettings().frameRate;
-      let canvas, ctx, t1 = performance.now();
-
       this.readable = new ReadableStream({
         async start(controller) {
           await loaded;
-          canvas = new OffscreenCanvas(video.videoWidth, video.videoHeight);
-          ctx = canvas.getContext('2d', {desynchronized: true});
+          this.canvas = new OffscreenCanvas(video.videoWidth, video.videoHeight);
+          this.ctx = canvas.getContext('2d', {desynchronized: true});
+          this.t1 = performance.now();
         },
         async pull(controller) {
-          while (performance.now() - t1 < interval) {
+          while (performance.now() - this.t1 < 1000 / track.getSettings().frameRate) {
             await new Promise(r => requestAnimationFrame(r));
           }
-          t1 = performance.now();
+          this.t1 = performance.now();
           ctx.drawImage(video, 0, 0);
-          controller.enqueue(new VideoFrame(canvas, {timestamp: t1}));
+          controller.enqueue(new VideoFrame(canvas, {timestamp: this.t1}));
         }
       });
     }
