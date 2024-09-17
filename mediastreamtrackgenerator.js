672867f4aa63f2dd4d98f19a9262c1733300c99e
Jan-Ivar Bruaroey <jan-ivar@users.noreply.github.com>
2024-09-17 09:10:57 -0400

    fix mediastreamtrackgenerator.js

--- mediastreamtrackgenerator.js
+++ mediastreamtrackgenerator.js
@@ -1,17 +1,46 @@
 if (!window.MediaStreamTrackGenerator) {
   window.MediaStreamTrackGenerator = class MediaStreamTrackGenerator {
-    constructor() {
-      const canvas = document.createElement("canvas");
-      const ctx = canvas.getContext('2d', {desynchronized: true});
-      this.track = canvas.captureStream().getVideoTracks()[0];
-      this.writable = new WritableStream({
-        write: frame => {
-          canvas.width = frame.displayWidth;
-          canvas.height = frame.displayHeight;
-          ctx.drawImage(frame, 0, 0, canvas.width, canvas.height);
-          frame.close();
-        },
-      });
+    constructor(kind) {
+      if (kind == "video") {
+        const canvas = document.createElement("canvas");
+        const ctx = canvas.getContext('2d', {desynchronized: true});
+        const track = canvas.captureStream().getVideoTracks()[0];
+        track.writable = new WritableStream({
+          write: frame => {
+            canvas.width = frame.displayWidth;
+            canvas.height = frame.displayHeight;
+            ctx.drawImage(frame, 0, 0, canvas.width, canvas.height);
+            frame.close();
+          }
+        });
+        return this.track;
+      } else if (kind == "audio") {
+        const ac = new AudioContext;
+        const generator = this;
+        const dest = createMediaStreamDestination();
+        this.track = dest.stream.getAudioTracks()[0];
+        this.writable = new WritableStream({
+          async start(controller) {
+            this.buffered = [];
+            function worklet() {
+              registerProcessor("mstg-shim", class Processor extends AudioWorkletProcessor {
+                  process(input, output) {
+                    this.port.postMessage(input);
+                    return true;
+                  }
+              });
+            }
+            await ac.audioWorklet.addModule(`data:text/javascript,(${worklet.toString()})()`);
+            this.node = new AudioWorkletNode(ac, "mstp-shim");
+            this.node.connect(dest);
+          },
+          write: audioData => {
+            this.node.port.postMessage("message", ({data}) => data[0][0] && this.buffered.push(data));
+            ctx.drawImage(frame, 0, 0, canvas.width, canvas.height);
+            audioData.close();
+          },
+        });
+      }
     }
   };
 }
