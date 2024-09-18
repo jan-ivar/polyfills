ddd814ef69f92f2f4e66efe976c3f284508b53e8
Jan-Ivar Bruaroey <jan-ivar@users.noreply.github.com>
2024-09-18 15:58:48 -0400

    fix

--- mediastreamtrackprocessor.js
+++ mediastreamtrackprocessor.js
@@ -47,7 +47,7 @@ if (!self.MediaStreamTrackProcessor) {
               numberOfFrames: channels[0].length,
               numberOfChannels: channels.length,
               timestamp: this.ac.currentTime * 1e6 | 0,
-              data: joined
+              data: joined,
               transfer: [joined.buffer]
             }));
           }
